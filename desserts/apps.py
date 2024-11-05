f8bca09653a38345807fcf333df99eb7395fbf8e
Liliya <liliyakhu@ukr.net>
2024-11-05 17:06:24 +0200

    Change ' on " using black package

--- desserts/apps.py
+++ desserts/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DessertsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'desserts'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "desserts"
