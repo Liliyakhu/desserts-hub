583d533f6e35ed67884ac6c8bfc7d807adfda25f
Liliya <liliyakhu@ukr.net>
2024-11-07 13:02:15 +0200

    Add index page (urls, view, templates)

--- desserts/views.py
+++ desserts/views.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from desserts.models import DessertType, Cook, Ingredient, Dessert
+# from desserts.forms import (
+#     DriverCreationForm,
+#     DriverLicenseUpdateForm,
+#     CarForm,
+#     DriverSearchForm,
+#     CarSearchForm,
+#     ManufacturerSearchForm
+# )
+
+
+@login_required
+def index(request):
+    """View function for the home page of the site."""
+
+    num_cooks = Cook.objects.count()
+    num_desserts = Dessert.objects.count()
+    num_dessert_types = DessertType.objects.count()
+    num_ingredients = Ingredient.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_cooks": num_cooks,
+        "num_desserts": num_desserts,
+        "num_dessert_types": num_dessert_types,
+        "num_ingredients": num_ingredients,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "desserts/index.html", context=context)
